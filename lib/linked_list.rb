0ce9407a6025cbf90bddaaa096cfafeaec0dcb60
Sam Lim <limsammy1@gmail.com>
2017-03-20 12:38:44 -0600

    Iteration 2 completed

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,30 +1,48 @@
 require_relative 'node'
 require 'pry'
 
-class LinkedList < Node
+class LinkedList
   attr_reader :head
-
-  attr_accessor :data
 
   def initialize
     @head = nil
   end
 
   def append(data)
-    @head = Node.new(data)
+    if head.nil?
+      @head = Node.new(data)
+    else
+      get_current_node(data)
+    end
   end
 
   def count
     current_node = @head
     count = 1
-    until current_node.next_node == nil
-      current_node = current.next_node
+    until current_node.next_node.nil?
+      current_node = current_node.next_node
       count += 1
     end
     count
   end
 
   def to_string
+    elements = ""
+    elements << @head.data
+    current_node = @head.next_node
+    until current_node.nil?
+      elements << " " + current_node.data
+      current_node = current_node.next_node
+    end
+    elements
+  end
+
+  def get_current_node(data)
+    current_node = @head
+    until current_node.next_node.nil?
+      current_node = current_node.next_node
+    end
+    current_node.next_node = Node.new(data)
   end
 end
 
