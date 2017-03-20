e19b17d8551f6daed3b7f1b16ecf10c7cd61f6e3
Sam Lim <limsammy1@gmail.com>
2017-03-20 10:30:39 -0600

    completed Iteration 0

---
+++ lib/node.rb
@@ -0,0 +1,12 @@
+class Node
+  attr_accessor :data, :next_node
+
+  def initialize(data, next_node = nil)
+    @data = data
+    @next_node = next_node
+  end
+end
+
+node = Node.new("plop")
+p node.data
+p node.next_node
