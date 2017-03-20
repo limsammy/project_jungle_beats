09260c2670ee69b7c9c56ee18ad6944a403da869
Sam Lim <limsammy1@gmail.com>
2017-03-20 11:26:47 -0600

    implemented append and count methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,17 +1,31 @@
-require './lib/node'
+require_relative 'node'
 require 'pry'
 
-class LinkedList
+class LinkedList < Node
   attr_reader :head
+
+  attr_accessor :data
 
   def initialize
     @head = nil
   end
 
   def append(data)
-    new_node = Node.new(data)
+    @head = Node.new(data)
+  end
+
+  def count
+    current = @head
+    count = 0
+    until current.next_node == nil
+      current = current.next_node
+      count += 1
+    end
+    count
+  end
+
+  def to_string
   end
 end
 
-list = LinkedList.new
 binding.pry
