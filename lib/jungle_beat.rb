cf3a91bc31ee3ec002a6679548642c6d223ee0f5
Sam Lim <limsammy1@gmail.com>
2017-03-20 22:44:48 -0600

    implemented valid? method. TODO: add that to append

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,4 +1,5 @@
 require_relative 'linked_list'
+require 'pry'
 
 class JungleBeat
   attr_reader :list
@@ -13,6 +14,18 @@ class JungleBeat
   def append(data)
     data.split(" ").each do |element|
       list.append(element)
+    end
+  end
+
+  def valid?(element)
+    valid = []
+    Dir.chdir(File.dirname(__FILE__))
+    valid_words = File.open("valid.txt", "r")
+    valid << valid_words.read.split(" ")
+    if valid[0].include?(element)
+      true
+    else
+      false
     end
   end
 
