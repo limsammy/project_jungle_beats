f1e1b1b0f272a633cd258cb5fef91bb91a126ee6
Sam Lim <limsammy1@gmail.com>
2017-03-21 10:23:19 -0600

    using correct test eval

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -6,7 +6,7 @@ require '../lib/linked_list'
 class LinkedListTest < MiniTest::Test
   def test_linked_list_is_created
     list = LinkedList.new
-    assert_equal list.class.to_s, "LinkedList"
+    assert_instance_of LinkedList, list
   end
 
   def test_head_is_nil
